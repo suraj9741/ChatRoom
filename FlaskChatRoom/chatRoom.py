29b009582f72bb2ad3bd0b288dec3b2fec7ad00b
suraj9741 <surajjadhav9741@gmail.com>
2020-12-03 08:19:37 +0530

    [Adding] chat html page for sending messages

--- FlaskChatRoom/chatRoom.py
+++ FlaskChatRoom/chatRoom.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/chat')
+def chat():
+    # request.args is library that contain url and data
+    username = request.args.get('username')
+    room = request.args.get('room')
+
+    if username and room:
+        return render_template('chat.html', username=username, room=room)
+    else:
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run()
